b85ee117cd081ff4daf7669d2fc3b88c88345778
jememouse <jememouse@gmail.com>
2025-09-23 10:40:29 +0800

    更新文件和配置

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html"],
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{ts,tsx,js,jsx,html}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          600: '#1e3a8a',
+          500: '#3b82f6',
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
